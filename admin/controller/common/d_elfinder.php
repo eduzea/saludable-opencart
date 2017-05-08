2a8fd659d491d1a70e1eb2ebcf3b22d46ec6b061
Eduardo Zea <eduzea@gmail.com>
2017-05-08 22:08:55 +0000

    Visual Designer and prerequisites

---
+++ admin/controller/common/d_elfinder.php
@@ -0,0 +1,13 @@
+<?php
+class ControllerCommonDElfinder extends Controller {
+	private $error = array();
+
+	public function connector() {
+		require_once(DIR_SYSTEM . 'library/d_elfinder/connector.php');
+		
+		header('Access-Control-Allow-Origin: *');
+		$connector = new elFinderConnector(new elFinder($opts), true);
+		$connector->run();
+	}
+
+}
